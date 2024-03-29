fb2f54de155f124cc3827e9d6344f45bf5d9497e
Ondřej Schlaichert <schlaichert.o@gmail.com>
2024-03-29 13:43:23 +0100

    Added error checking

--- src/endpoints/get-conversation-by-id.js
+++ src/endpoints/get-conversation-by-id.js
@@ -1,5 +1,5 @@
 /**
- * Handles the /get-conversation endpoint
+ * Handles the /get-conversation-by-id endpoint
  */
 
 const conversations = require('../database/gateways/conversation-gateway');
@@ -11,9 +11,23 @@ const router = express.Router();
 router.get('/:id', async function (req, res) {
 	const id = req.params.id;
 
+	const conversation = await conversations.getConversationById(id);
+
+	if (conversation == null) {
+		res.status(400);
+		res.json({
+			error: 400,
+			message: 'CONVERSATION DOES NOT EXIST',
+		});
+		logger.warning(
+			`${req.method} fail: Attempted to get nonexistent conversation at ${req.originalUrl}. (${req.ip})`
+		);
+		return;
+	}
+
 	logger.success(`${req.method} OK: ${req.originalUrl} (${req.ip})`);
 	res.status(200);
-	res.json(await conversations.getConversationById(id));
+	res.json();
 });
 
 module.exports = router; 
